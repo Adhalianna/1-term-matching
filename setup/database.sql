b105810194bac3c463ef51d3bcdc8acf2fee04ff
Natalia Goc <natka.goc@gmail.com>
2021-05-22 22:15:16 +0200

    altering schemas in the database

--- setup/database.sql
+++ setup/database.sql
@@ -1,5 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS dicts;
-CREATE SCHEMA IF NOT EXISTS docs;
 
-ALTER ROLE term_matcher SET search_path TO dicts, docs;
+ALTER ROLE term_matcher SET search_path TO dicts;
 
+CREATE TABLE IF NOT EXISTS docs(
+    id serial primary key,
+    title varchar(255), --optional field
+    document text not null,
+);
