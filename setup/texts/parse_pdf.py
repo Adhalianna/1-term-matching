47b13dc827399f14b22ba1e09f069447c9e03c25
NKGoc <natka.goc@gmail.com>
2021-06-15 21:41:12 +0200

    more automated text generation

--- setup/texts/parse_pdf.py
+++ setup/texts/parse_pdf.py
@@ -3,6 +3,7 @@ import psycopg2
 import pdftotext
 import sys
 import re
+import math
 
 # Get filename from arguments
 try:
@@ -13,38 +14,52 @@ except IndexError:
     First, pass a name of a pdf file that is to be parsed and uploaded to the database. 
     Second, add a title that will be inserted into the database""")
 
+if sys.argv[3] is not None:
+    sizes = int(sys.argv[3])
+else:
+    sizes = 1
+
+
 with open(filename, "rb") as f:
     text = pdftotext.PDF(f)
 
-print("Found a pdf file with " + str(len(text)) + " pages.")
+page_num = len(text)
 
-# Remove whitespaces all over the places to save yourself some time and memory
-for page in text:
-    page.strip()
+print("Found a pdf file with " + str(page_num) + " pages. It will be partitioned to create " + str(sizes) + " texts in the database.")
 
-text = "\n\n".join(text)
-
-white_space_strip = re.compile(r'\s{2,}|\n')
-text = re.sub(white_space_strip, ' ', text)
+partition_size = math.floor(page_num / sizes)
+print("The smalles text will use " + str(partition_size) + " pages.")
 
 
-print("The inserted version of the text will have " + str(len(text)) + " characters.")
+for i in range(sizes):
+    # Remove whitespaces all over the places to save yourself some time and memory
+    max_pages = (i + 1) * partition_size
 
-# Database 
-try:
-    db = psycopg2.connect("dbname=term_matching_db user=term_matcher password=term_matcher")
-except:
-    print("Failed to connect to the database.")
+    result_text = []
+    for j in range(max_pages):
+        result_text.append(text[j])
+    result_text = "\n\n".join(result_text)
 
-# Opening a cursor that performs database operations
-cur = db.cursor()
+    white_space_strip = re.compile(r'\s{2,}|\n')
+    result_text = re.sub(white_space_strip, ' ', result_text)
 
-# Insert
-try:
-    text_to_insert = (text,)
-    cur.execute("INSERT INTO docs(document, title, ts_tokens) VALUES(%s, %s, to_tsvector(%s))", (text_to_insert, title, text_to_insert))
-    db.commit()
-except:
-    print("Failed to insert the text into the database.")
+    print("The inserted version of the text will have " + str(len(result_text)) + " characters.")
 
-print("Data successfully committed to the database!")
+    # Database 
+    try:
+        db = psycopg2.connect("dbname=term_matching_db user=term_matcher password=term_matcher")
+    except:
+        print("Failed to connect to the database.")
+
+    # Opening a cursor that performs database operations
+    cur = db.cursor()
+
+    # Insert
+    try:
+        text_to_insert = (result_text,)
+        cur.execute("INSERT INTO docs(document, title, ts_tokens) VALUES(%s, %s, to_tsvector(%s))", (text_to_insert, title + "_" + str(i), text_to_insert))
+        db.commit()
+    except:
+        print("Failed to insert the text into the database.")
+
+    print("Data successfully committed to the database!")
