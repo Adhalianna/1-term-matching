0213c34ff09ad24ee4deb0e17a80fcd24cabc0be
NKGoc <natka.goc@gmail.com>
2021-06-15 04:11:03 +0200

    new sets of tests

--- benchmarks/data2/prepare_dataset2.sh
+++ benchmarks/data2/prepare_dataset2.sh
@@ -15,37 +15,36 @@ case $input in
 esac
 
 # Clear-up at start
-echo "DROP TABLE IF EXISTS dicts.wiki_magic;" | psql -d term_matching_db -U term_matcher -q
-echo "DROP TABLE IF EXISTS dicts.wiki_magic_medium;" | psql -d term_matching_db -U term_matcher -q
-echo "DROP TABLE IF EXISTS dicts.wiki_magic_small;" | psql -d term_matching_db -U term_matcher -q
+echo "DROP TABLE IF EXISTS dicts.wiki_cogn;" | psql -d term_matching_db -U term_matcher -q
+echo "DROP TABLE IF EXISTS dicts.wiki_cogn_medium;" | psql -d term_matching_db -U term_matcher -q
+echo "DROP TABLE IF EXISTS dicts.wiki_cogn_small;" | psql -d term_matching_db -U term_matcher -q
 
 # Create the base (full, big) dictionary 
-./setup/dictionaries/wikigraph.py Biology 5 wiki_biology en
+./setup/dictionaries/wikigraph.py "Embodied cognition" 2 wiki_cogn en
 
-echo "UPDATE dicts.wiki_biology " \
-"SET term_query = phraseto_tsquery(dicts.wiki_biology.term);" | psql -d term_matching_db -U term_matcher -q # wikigraph.py was update to create tsquery on insert so this is unnecessary (or not?)
+echo "UPDATE dicts.wiki_cogn " \
+"SET term_query = phraseto_tsquery(dicts.wiki_cogn.term);" | psql -d term_matching_db -U term_matcher -q # wikigraph.py was update to create tsquery on insert so this is unnecessary (or not?)
 
 
 # Create a medium sized dictionary having half the entries of the original
-echo "CREATE TABLE dicts.wiki_biology_medium AS" \
+echo "CREATE TABLE dicts.wiki_cogn_medium AS" \
 "SELECT id, term, term_query, term_vector, definition" \
-"FROM dicts.wiki_biology" \
+"FROM dicts.wiki_cogn" \
 "WHERE id % 2 = 0;" | psql -d term_matching_db -U term_matcher -q
 
 # Create a small dictionary that has half the size of the medium one
-echo "CREATE TABLE dicts.wiki_biology_small AS" \
+echo "CREATE TABLE dicts.wiki_cogn_small AS" \
 "SELECT id, term, term_query, term_vector, definition" \
-"FROM dicts.wiki_biology" \
+"FROM dicts.wiki_cogn" \
 "WHERE id % 4 = 0;" | psql -d term_matching_db -U term_matcher -q
 
 
 # Import texts
-./setup/texts/parse_pdf.py setup/texts/Brave_New_World.pdf BNW_full
-./setup/texts/parse_pdf.py setup/texts/Brave_New_World_short.pdf BNW_short
+./setup/texts/parse_pdf.py setup/texts/Einstein_Relativity.pdf Relativity
 
 # Create new dictionaries to be used internally by Postgres
-sudo ./setup/dictionaries/generate_postgresdict.py wiki_biology "$(pg_config --sharedir)/tsearch_data"
-sudo ./setup/dictionaries/generate_postgresdict.py wiki_biology_medium "$(pg_config --sharedir)/tsearch_data"
-sudo ./setup/dictionaries/generate_postgresdict.py wiki_biology_small "$(pg_config --sharedir)/tsearch_data"
+sudo ./setup/dictionaries/generate_postgresdict.py wiki_cogn "$(pg_config --sharedir)/tsearch_data"
+sudo ./setup/dictionaries/generate_postgresdict.py wiki_cogn_medium "$(pg_config --sharedir)/tsearch_data"
+sudo ./setup/dictionaries/generate_postgresdict.py wiki_cogn_small "$(pg_config --sharedir)/tsearch_data"
 
 
---
+++ benchmarks/data2/tests_set03.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+
+small_dict="wiki_cogn_small"
+medium_dict="wiki_cogn_medium"
+big_dict="wiki_cogn"
+
+dictionaries=("$small_dict" "$medium_dict" "$big_dict")
+
+
+short_text="BNW_short"
+long_text1="BNW_full"
+long_text2="Relativity"
+
+documents=("$short_text" "$long_text1" "$long_text2")
+
+#---------------------------------------------------------------
+
+declare -A stats
+
+#this part somewhat violates "do not repeat yourself" rule... 
+small_dict_entries=`echo "SELECT count(id) FROM dicts.$small_dict" \
+    | psql -d term_matching_db -U term_matcher \
+    | grep -m 1 -Eo '[0-9]{1,9}'`
+stats[$small_dict]="$small_dict_entries"
+
+medium_dict_entries=`echo "SELECT count(id) FROM dicts.$medium_dict" \
+    | psql -d term_matching_db -U term_matcher \
+    | grep -m 1 -Eo '[0-9]{1,9}'`
+stats[$medium_dict]="$medium_dict_entries"
+
+big_dict_entries=`echo "SELECT count(id) FROM dicts.$big_dict" \
+    | psql -d term_matching_db -U term_matcher \
+    | grep -m 1 -Eo '[0-9]{1,9}'`
+stats[$big_dict]="$big_dict_entries"
+
+short_text_words=`echo "SELECT regexp_split_to_table(lower(docs.document), " \
+    "'([\.\;\,\:\?\"]*[[:space:]]+|\.)') tokens " \
+    "INTO TEMPORARY words " \
+    "FROM docs " \
+    "WHERE docs.title = '$short_text'; " \
+    "SELECT count(*) " \
+    "FROM words;" \
+    | psql -d term_matching_db -U term_matcher \
+    | grep -m 2 -Eo '[0-9]{1,9}' \
+    | tail -n 1`
+stats[$short_text]="$short_text_words"
+
+long_text1_words=`echo "SELECT regexp_split_to_table(lower(docs.document), " \
+    "'([\.\;\,\:\?\"]*[[:space:]]+|\.)') tokens " \
+    "INTO TEMPORARY words " \
+    "FROM docs " \
+    "WHERE docs.title = '$long_text1'; " \
+    "SELECT count(*) " \
+    "FROM words;" \
+    | psql -d term_matching_db -U term_matcher \
+    | grep -m 2 -Eo '[0-9]{1,9}' \
+    | tail -n 1`
+stats[$long_text1]="$long_text1_words"
+
+long_text2_words=`echo "SELECT regexp_split_to_table(lower(docs.document), " \
+    "'([\.\;\,\:\?\"]*[[:space:]]+|\.)') tokens " \
+    "INTO TEMPORARY words " \
+    "FROM docs " \
+    "WHERE docs.title = '$long_text2'; " \
+    "SELECT count(*) " \
+    "FROM words;" \
+    | psql -d term_matching_db -U term_matcher \
+    | grep -m 2 -Eo '[0-9]{1,9}' \
+    | tail -n 1`
+stats[$long_text2]="$long_text2_words"
+
+
+#---------------------------------------------------------------
+
+_test() {
+    local test_collection=$1
+    local test_number=$2
+    local query=$3 
+    local dict=$4
+    local doc=$5
+    local uses_count=$6
+
+    local test_name="$1-$2"
+    local entries=${stats[$dict]}
+    local words=${stats[$doc]}
+
+    # NOTE: Check loose_notes.md!
+    ./setup/dictionaries/configure_dict.sh $dict "dicts_config"
+    echo "UPDATE $dict SET term_query = phraseto_tsquery('dicts_config', term);" | psql -d term_matching_db -U term_matcher -q
+    # NOTE: Check loose_notes.md!
+
+    local results=$(echo "\timing on \\\ ${query}" | psql -d term_matching_db -U term_matcher)
+
+    local count=$(grep -Eo "[0-9]*" <<< $results | head -n 1)
+    local time=$(grep -Eo "[0-9]*[,\.][0-9]* ms" <<< $results | tail -n 1)
+    
+    echo "[TEST $test_name] $entries dictionary entries ($dict) | $words text words ($doc) | $count matches | $time"
+
+    local formatted_time=$(grep -m 1 -Eo "[0-9]*.[0-9]*" <<< $time)
+    local formatted_time=$(echo $formatted_time | awk '{print int($1)}')
+    local formatted_time="$formatted_time millisecond"
+    if [ $uses_count = true ] ; then
+        echo "INSERT INTO tests VALUES (DEFAULT, '$test_name', now(), '$test_collection', '$dict', $entries, '$doc', $words, INTERVAL '$formatted_time', $count);" | psql -d term_matching_db -U term_matcher
+    else
+        echo "INSERT INTO tests VALUES (DEFAULT, '$test_name', now(), '$test_collection', '$dict', $entries, '$doc', $words, INTERVAL '$formatted_time', -1);" | psql -d term_matching_db -U term_matcher
+    fi
+}
+
+_test_case() {
+    local collection_name=$1
+    local description=$2
+    local query=$3
+    local uses_count=${4:-true}
+
+    local query_insertable=$(echo "$query" | tr -s " ")
+    local query_insertable=${query_insertable//\'/\'\'}
+    echo $query_insertable
+    sleep 5
+    echo "INSERT INTO test_collections VALUES ('$collection_name', '$description', '${query_insertable}')" | psql -d term_matching_db -U term_matcher -q
+
+
+    counter="0"
+    for i in "${dictionaries[@]}"; do
+        for j in "${documents[@]}"; do
+            counter=$((counter + 1))
+            local dict=$i
+            local doc=$j
+            local test_query=${query//DICT/$dict}
+            local test_query=${test_query//DOC/$doc}
+            _test "$collection_name" "$counter" "$test_query" "$dict" "$doc" "$uses_count"
+        done
+    done
+}
+
+#---------------------------------------------------------------
+
+# TEST 3-1
+
+q1=`echo "SELECT to_tsvector('dicts_config', docs.document) " \
+"FROM docs " \
+"WHERE docs.title = 'DOC';"`
+
+_test_case "3-1" "Generates a tsvector using a modified  text-search dictionary." "${q1}"
+
+
+#---------------------------------------------------------------
+
+# TEST 3-2
+
+q2=`echo "SELECT count(dicts.DICT.id) " \
+"FROM dicts.DICT, docs " \
+"WHERE to_tsvector('dicts_config', docs.document) @@ dicts.DICT.term_query" \
+"AND docs.title = 'DOC';"`
+
+_test_case "3-2" "The text is parsed to a tsvector and each dictionary entry is used in the form of a previously prepared tsquery. Text search functions use a previously prepared text-search dictionary." "${q2}"
+
+#---------------------------------------------------------------
+
+# TEST 3-3
+
+#Creating the index:
+echo "CREATE INDEX btree_${big_dict}_indx ON dicts.${big_dict} USING GIST (term_query);" | psql -d term_matching_db -U term_matcher -q
+echo "CREATE INDEX btree_${medium_dict}_indx ON dicts.${medium_dict} USING GIST (term_query);" | psql -d term_matching_db -U term_matcher -q
+echo "CREATE INDEX btree_${small_dict}_indx ON dicts.${small_dict} USING GIST (term_query);" | psql -d term_matching_db -U term_matcher -q
+
+q3=`echo "SELECT count(dicts.DICT.id) " \
+"FROM dicts.DICT, docs " \
+"WHERE to_tsvector('dicts_config', docs.document) @@ dicts.DICT.term_query" \
+"AND docs.title = 'DOC';"`
+
+_test_case "3-3" "The text is parsed to a tsvector and each dictionary entry is used in the form of a previously prepared tsquery. This case uses a GIST index on the tsquery. Text search functions use a previously prepared text-search dictionary." "${q3}"
+
+echo "DROP INDEX btree_${big_dict}_indx ON dicts.${big_dict};" | psql -d term_matching_db -U term_matcher -q
+echo "DROP INDEX btree_${medium_dict}_m_indx ON dicts.${medium_dict};" | psql -d term_matching_db -U term_matcher -q
+echo "DROP INDEX btree_${small_dict}_indx ON dicts.${small_dict};" | psql -d term_matching_db -U term_matcher -q
+
+#---------------------------------------------------------------
+
+# TEST 3-4
+
+q4=`echo "SELECT to_tsvector('dicts_config', docs.document) @@ to_tsquery('dicts_config', " \
+"array_to_string( " \
+"ARRAY( " \
+"SELECT dicts.DICT.term " \
+"FROM dicts.DICT " \
+"), " \
+"' | ' " \
+")::text " \
+") AS does_it_contain " \
+"FROM docs " \
+"WHERE docs.title = 'DOC';"`
+
+_test_case "3-4" "The whole dictionary is transformed into a single tsquery. It tells only whether there are any matches. Text search functions use a previously prepared text-search dictionary." "${q4}" "false"
+
+#---------------------------------------------------------------
+
+# TEST 3-5
+
+q5=`echo "SELECT " \
+"ts_headline(docs.document, phraseto_tsquery('dicts_config', dicts.DICT.term))" \
+"WHERE docs.ts_tokens @@ dicts.DICT.term_query"` \
+"AND docs.title = 'DOC'"
+
+_test_case "3-5" "A Postgres function ts_headline is used to show matches inside the text. Terms are used as previously prepared queries. Text search functions use a previously prepared text-search dictionary." "${q5}" "false"
